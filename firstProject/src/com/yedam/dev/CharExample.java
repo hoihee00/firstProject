58e03d7f5aab2a1eccacf3a275de5cdb412b7f3e
Dahee <hoihee00@naver.com>
2020-08-11 15:39:44 +0900

    첫번째 커밋

---
+++ firstProject/src/com/yedam/dev/CharExample.java
@@ -0,0 +1,14 @@
+package com.yedam.dev;
+
+// com.yedam.dev.CharExamlpe
+public class CharExample {
+	public static void main(String[] args) {
+//		char c1 = 44032; //65에 해당되는 unicode 값 => A
+//		for(int i=0; i<26; i++) {
+////			System.out.print(c1++ + "\t");		
+//		}
+
+		char c2 = '나';
+		System.out.println((byte) c2);
+	}
+}
